742a9afc1b6881e59c7a7dbcd30959da8eb381de
roman3222 <romkakms3222@gmail.com>
2023-09-27 22:13:19 +1000

    add model and add CustomUserAdmin in admin.py

--- shop_api/backend/admin.py
+++ shop_api/backend/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from backend.models import User, Shop, Category, Product, ProductInfo, Parameter, ProductParameter, Cart, CartItem, \
+    Order, OrderItem, Contacts, ConfirmEmailToken
+
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    """
+    Панель управления пользователями
+    """
+    model = User
+    fieldsets = (
+        (None, {'fields': ('email', 'password', 'type')}),
+        ('Personal info', {'fields': ('first_name', 'last_name', 'company', 'position')}),
+        ('Permissions', {
+            'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions'),
+        }),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+    )
+    list_display = ('email', 'first_name', 'last_name', 'is_staff')
+
+
--- shop_api/backend/models.py
+++ shop_api/backend/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import AbstractUser
 from django.contrib.auth.validators import UnicodeUsernameValidator
 from django.db import models
 from django.utils.translation import gettext_lazy as _
+from django_rest_passwordreset.tokens import get_token_generator
 
 STATE_CHOICES = (
     ('basket', 'Статус корзины'),
@@ -49,6 +50,7 @@ class UserManager(BaseUserManager):
     def create_superuser(self, email, password, **extra_fields):
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
+
         if extra_fields.get('is_staff') is not True:
             raise ValueError('Superuser must have is_staff=True')
         if extra_fields.get('is_superuser') is not True:
@@ -85,7 +87,7 @@ class User(AbstractUser):
     )
     is_active = models.BooleanField(
         _('active'),
-        default=False,
+        default=True,
         help_text=_(
             'Designates whether this user should be treated as active.'
             'Unselect this instead of deleting accounts.'
@@ -157,7 +159,7 @@ class ProductInfo(models.Model):
         verbose_name = 'Информация о продукте'
         verbose_name_plural = 'Информационный список о продуктах'
         constraints = [
-            models.UniqueConstraint(fields=['product', 'shop', 'external_id'], name = 'unique_product_info'),
+            models.UniqueConstraint(fields=['product', 'shop', 'external_id'], name='unique_product_info'),
         ]
 
 
@@ -209,10 +211,39 @@ class Contacts(models.Model):
         return f'{self.city} {self.street} {self.house}'
 
 
+class Cart(models.Model):
+    user = models.OneToOneField(User, verbose_name='Пользователь', null=True, blank=True,
+                                on_delete=models.CASCADE)
+    products = models.ManyToManyField(Product, through='CartItem', related_name='carts')
+    quantity = models.PositiveIntegerField(verbose_name='Количество товаров', default=0)
+    created_time = models.DateTimeField(auto_now_add=True, blank=True, null=True)
+
+    class Meta:
+        verbose_name = 'Корзина'
+        verbose_name_plural = 'Список товаров в корзине'
+
+    def __str__(self):
+        return f'{self.products} - {self.quantity} - {self.created_time}'
+
+
+class CartItem(models.Model):
+    cart = models.ForeignKey(Cart, verbose_name='Корзина', related_name='cart_items',
+                             on_delete=models.CASCADE, blank=True)
+    product = models.ForeignKey(Product, verbose_name='Продукт', related_name='cart_items',
+                                on_delete=models.CASCADE, blank=True)
+
+    class Meta:
+        verbose_name = 'Позиция корзины'
+        verbose_name_plural = 'Список позиций корзины'
+
+
 class Order(models.Model):
     user = models.ForeignKey(User, verbose_name='Пользователь',
                              related_name='orders', blank=True,
                              on_delete=models.CASCADE)
+    cart = models.ForeignKey(Cart, verbose_name='Корзина',
+                             related_name='orders', blank=True,
+                             on_delete=models.CASCADE, default=None)
     dt = models.DateTimeField(auto_now=True)
     state = models.CharField(verbose_name='Статус', choices=STATE_CHOICES, max_length=20)
     contacts = models.ForeignKey(Contacts, verbose_name='Контакт',
@@ -234,6 +265,7 @@ class OrderItem(models.Model):
     product_info = models.ForeignKey(ProductInfo, verbose_name='Информация о продукте', related_name='ordered_items',
                                      on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField(verbose_name='Количество')
+    price = models.PositiveIntegerField(verbose_name='Цена(на момент создания заказа)', blank=True, null=True)
 
     class Meta:
         verbose_name = 'Заказная позиция'
@@ -242,5 +274,49 @@ class OrderItem(models.Model):
             models.UniqueConstraint(fields=['order_id', 'product_info'], name='unique_order_item')
         ]
 
+    def sum_price(self):
+        quantity = float(self.quantity)
+        price = float(self.price)
+        return quantity * price
+
+    def __str__(self):
+        return (f'{self.order} - {self.product_info} - {self.quantity} - {self.price}'
+                f' - {self.sum_price()}')
 
 
+class ConfirmEmailToken(models.Model):
+    class Meta:
+        verbose_name = 'Токен подтверждения Email'
+        verbose_name_plural = 'Токены подтверждения Email'
+
+    @staticmethod
+    def generate_key():
+        """generates a pseudo random code using os.urandom and binascii.hexlify"""
+        return get_token_generator().generate_token()
+
+    user = models.ForeignKey(
+        User,
+        related_name='confirm_email_token',
+        on_delete=models.CASCADE,
+        verbose_name=_('The User which is associated to this password reset token')
+    )
+
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name=_('When was this token generated')
+    )
+
+    key = models.CharField(
+        _('key'),
+        max_length=64,
+        db_index=True,
+        unique=True
+    )
+
+    def save(self, *args, **kwargs):
+        if not self.key:
+            self.key = self.generate_key()
+        return super(ConfirmEmailToken, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return 'Password reset token for user {user}'.format(user=self.user)
